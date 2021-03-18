d2061c778f620c6a96c06854194a29d2138b6f31
wimaar2021daw2 <wimaar2021daw2@protonmail.com>
2021-03-18 15:19:36 +0100

    Error de ortografía

--- view/inicio.php
+++ view/inicio.php
@@ -5,7 +5,7 @@ if($sessionStatus) header('location: /prjm08uf23/?page=dashboard');
 $title = 'Inicio';
 $html =' 
     <main>
-        <h1>Bienvenidos a la página de inicion</h1>
+        <h1>Bienvenidos a la página de inicio</h1>
         <ul>
             <li><a href="/prjm08uf23?page=login">Iniciar sesión</a></li>
         </ul>
